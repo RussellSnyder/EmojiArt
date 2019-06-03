874d641ef3043962df27b362e5322d2ef80d03fe
Russell Snyder <russell@rustybass.com>
2019-06-03 15:55:09 +0200

    Initial Commit

---
+++ EmojiArt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiArt
+//
+//  Created by Russell Wunder on 03.06.19.
+//  Copyright © 2019 Russell Snyder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
